6f0273fe00c4dfc2652fffb5cba882a77f45b4eb
Kai Reinhard <K.Reinhard@micromata.de>
2021-04-15 00:08:02 +0200

    Adapted to new versoin 7.1 (without tenants).

--- examples/java/org.projectforge.pluginexamples.jmemo/src/main/resources/flyway/jmemo/init/common/V1.0.0__Base_version.sql
+++ examples/java/org.projectforge.pluginexamples.jmemo/src/main/resources/flyway/jmemo/init/common/V1.0.0__Base_version.sql
@@ -8,7 +8,6 @@ CREATE TABLE t_plugin_jmemo (
   last_update TIMESTAMP WITHOUT TIME ZONE,
   memo        CHARACTER VARYING(4000),
   subject     CHARACTER VARYING(1000),
-  tenant_id   INTEGER,
   owner_fk    INTEGER
 );
 
@@ -17,12 +16,6 @@ ALTER TABLE t_plugin_jmemo
 
 CREATE INDEX idx_fk_t_plugin_jmemo_owner_fk
   ON t_plugin_jmemo (owner_fk);
-
-CREATE INDEX idx_fk_t_plugin_jmemo_tenant_id
-  ON t_plugin_jmemo (tenant_id);
-
-ALTER TABLE t_plugin_jmemo
-  ADD CONSTRAINT fkjm72ignhv3m4spo048i722roy FOREIGN KEY (tenant_id) REFERENCES t_tenant (pk);
 
 ALTER TABLE t_plugin_jmemo
   ADD CONSTRAINT fkjmub13xpknmrxc8402idlxw0d FOREIGN KEY (owner_fk) REFERENCES t_pf_user (pk);
--- examples/kotlin/org.projectforge.pluginexamples.ktmemo/src/main/resources/flyway/ktmemo/init/common/V1.0.0__Base_version.sql
+++ examples/kotlin/org.projectforge.pluginexamples.ktmemo/src/main/resources/flyway/ktmemo/init/common/V1.0.0__Base_version.sql
@@ -8,7 +8,6 @@ CREATE TABLE t_plugin_ktmemo (
   last_update TIMESTAMP WITHOUT TIME ZONE,
   memo        CHARACTER VARYING(4000),
   subject     CHARACTER VARYING(1000),
-  tenant_id   INTEGER,
   owner_fk    INTEGER
 );
 
@@ -17,12 +16,6 @@ ALTER TABLE t_plugin_ktmemo
 
 CREATE INDEX idx_fk_t_plugin_ktmemo_owner_fk
   ON t_plugin_ktmemo (owner_fk);
-
-CREATE INDEX idx_fk_t_plugin_ktmemo_tenant_id
-  ON t_plugin_ktmemo (tenant_id);
-
-ALTER TABLE t_plugin_ktmemo
-  ADD CONSTRAINT fkkt72ignhv3m4spo048i722roy FOREIGN KEY (tenant_id) REFERENCES t_tenant (pk);
 
 ALTER TABLE t_plugin_ktmemo
   ADD CONSTRAINT fkktub13xpknmrxc8402idlxw0d FOREIGN KEY (owner_fk) REFERENCES t_pf_user (pk);
